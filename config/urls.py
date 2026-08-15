27c6de049b4ade56df6b30bca323f1232712aa00
Landon George <landon.george16@gmail.com>
2026-08-15 12:59:47 -0600

    Fix media serving in production - static() helper silently no-ops when DEBUG=False

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/accounts/login/", permanent=False)),
     path("admin/", admin.site.urls),
     path("scans/", include("scans.urls")),
     path("accounts/", include("accounts.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
